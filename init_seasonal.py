25b7816121c164ea0745cf97ec1783384b40ff47
Alice <jaeyeon0921@gmail.com>
2024-07-04 15:51:57 +0900

    create: python file for inserting data

---
+++ init_seasonal.py
@@ -0,0 +1,53 @@
+import mysql.connector
+from mysql.connector import Error
+import csv
+
+def connect_to_database(host, database, user, password):
+    """ Connect to MySQL database """
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+        if connection.is_connected():
+            print("Successfully connected to the database")
+            return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
+
+def insert_data_from_csv(connection, csv_file_path, table_name):
+    """ Insert data from CSV file into MySQL table """
+    cursor = connection.cursor()
+    try:
+        with open(csv_file_path, mode='r') as file:
+            csv_reader = csv.reader(file)
+            header = next(csv_reader)  # Skip the header row
+            for row in csv_reader:
+                # Construct SQL statement with proper quoting and column names
+                sql = f"INSERT INTO {table_name} (seasonal_name, seasonal_month, seasonal_image) VALUES ('{row[0]}', {row[1]}, '{row[2]}')"
+                print(f"seasonal data {row[0]} inserted")
+                cursor.execute(sql)  # Execute the SQL statement
+            connection.commit()
+            print(f"Data from {csv_file_path} has been inserted into the {table_name} table.")
+    except Error as e:
+        print(f"Error while inserting data into MySQL table: {e}")
+    finally:
+        cursor.close()
+
+if __name__ == '__main__':
+    # Replace the placeholders with your MySQL server details
+    host = '127.0.0.1'
+    database = 'mlr-dev-db-tlb'
+    user = 'test'
+    password = 'test1234'
+    csv_file_path = 'data/seasonal.csv'
+    table_name = 'Seasonal'
+
+    connection = connect_to_database(host, database, user, password)
+    if connection:
+        insert_data_from_csv(connection, csv_file_path, table_name)
+        connection.close()
+        print("MySQL connection is closed")
